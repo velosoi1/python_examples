63aff8ee21997a4fcbb4c4ee4fa3cd9d0ab7c520
Your Name <you@example.com>
2023-06-08 22:51:41 +0000

    edited

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,20 +1,27 @@
+import nltk
+import numpy as np
+
+nltk.download("punkt")
+
 def extract_features(text):
     features = {}
-    features["clength"] = len(text)
+    text = text.strip()
+    features["length##" + str(int(np.log2(len(text) + 1)))] = 1
     tokens = nltk.tokenize.word_tokenize(text)
-    features["tlength"] = len(tokens)
-    for c in text:
-        features["c##" + c] = 1
-    for i in range(len(text)):
-        c_bigram = text[i:i + 2]
-        if len(c_bigram) !=2:
-            break
-        features["c##" + c_bigram] = 1
-    for token in tokens:
-        features["t##" + token] = 1
-    for i in range(len(tokens)):
-        w_bigram = tokens[i:i + 2]
-        if len(w_bigram) != 2:
-            break
-        features["t##" + " ".join(w_bigram)] = 1
+    text = text.lower()
+    for n in range(2, 6):
+        for i in range(len(text)):
+            c_ngram = text[i:i + n]
+            if len(c_ngram) != n:
+                break
+            features["cngram##" + c_ngram] = 1
+    for n in range(1, 6):
+        for i in range(len(tokens)):
+            w_ngram = tokens[i:i + n]
+            if len(w_ngram) != n:
+                break
+            features["wngram##" + " ".join(w_ngram).lower()] = 1
     return features
+
+if __name__ == "__main__":
+    print(extract_features("Hello, class!"))
